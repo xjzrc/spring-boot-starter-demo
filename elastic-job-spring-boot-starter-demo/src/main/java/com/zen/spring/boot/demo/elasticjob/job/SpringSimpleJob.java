ebb19ce2d57fdf4783706cffb6a9c9763f79c5b5
jinjing <jinjing@fcbox.com>
2018-04-12 12:58:09 +0800

    elastic-job-spring-boot-starter-demo替换成最新的stater，去除注解方式配置任务，改成配置文件方式配置任务

--- elastic-job-spring-boot-starter-demo/src/main/java/com/zen/spring/boot/demo/elasticjob/job/SpringSimpleJob.java
+++ elastic-job-spring-boot-starter-demo/src/main/java/com/zen/spring/boot/demo/elasticjob/job/SpringSimpleJob.java
@@ -2,23 +2,14 @@ package com.zen.spring.boot.demo.elasticjob.job;
 
 import com.dangdang.ddframe.job.api.ShardingContext;
 import com.dangdang.ddframe.job.api.simple.SimpleJob;
-import com.zen.elasticjob.spring.boot.annotation.ElasticJobConfig;
 import com.zen.spring.boot.demo.elasticjob.fixture.entity.Foo;
 import com.zen.spring.boot.demo.elasticjob.fixture.repository.FooRepository;
-import com.zen.spring.boot.demo.elasticjob.listener.MyDistributeElasticJobListener;
-import com.zen.spring.boot.demo.elasticjob.listener.MyElasticJobListener;
 
 import javax.annotation.Resource;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-@ElasticJobConfig(cron = "${spring.elasticjob.simple.cron}", shardingTotalCount = "${spring.elasticjob.simple.shardingTotalCount}",
-        shardingItemParameters = "${spring.elasticjob.simple.shardingItemParameters}",
-        listenerClass = "${spring.elasticjob.simple.listenerClass}",
-        distributedListenerClass = "${spring.elasticjob.simple.distributedListenerClass}",
-        startedTimeoutMilliseconds = "${spring.elasticjob.simple.startedTimeoutMilliseconds}",
-        completedTimeoutMilliseconds = "${spring.elasticjob.simple.completedTimeoutMilliseconds}")
 public class SpringSimpleJob implements SimpleJob {
 
     @Resource
